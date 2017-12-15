9f3ff34ff0f0c06f5ed0c5adc81753ce854456ad
Jakub Kret <jakub.a.kret@gmail.com>
2017-12-15 11:01:12 +0100

    Print highscores received from server

--- src/main/java/hints/main.java
+++ src/main/java/hints/main.java
@@ -2,6 +2,8 @@ package hints;
 
 import score.Score;
 
+import java.util.List;
+
 public class main {
     public static void main(String[] args) {
         Highscores highscores = new HighscoresHTTP();
@@ -9,6 +11,10 @@ public class main {
         highscores.addScore(new Score("aaa", 111111111));
         highscores.addScore(new Score("bbb", 222222222));
         highscores.addScore(new Score("ccc", 333333333));
-        highscores.getHighscores(0);
+        List<Score> scores = highscores.getHighscores(0);
+        for (Score score : scores) {
+            System.out.println(score.getName());
+            System.out.println(score.getScore());
+        }
     }
 }
