7411977ab83b86b856b0e65ec989dc3239481bf2
Jakub Kret <jakub.a.kret@gmail.com>
2017-12-15 10:35:27 +0100

    Use HighscoresHTTP in hints main class

--- src/main/java/hints/HighscoresHTTP.java
+++ src/main/java/hints/HighscoresHTTP.java
@@ -1,5 +1,6 @@
 package hints;
 
+import com.google.gson.Gson;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
@@ -18,19 +19,16 @@ public class HighscoresHTTP implements Highscores {
     @Override
     public void addScore(Score score) {
         HttpClient httpClient = new DefaultHttpClient();
+        Gson gson = new Gson();
 
         try {
-
             HttpPost request = new HttpPost("http://localhost:8080/score");
-            StringEntity params = new StringEntity(
-                    String.format("{\"name\": %s, \"score\": %d}",
-                            score.getName(),
-                            score.getScore()));
+            StringEntity params = new StringEntity(gson.toJson(score));
             request.addHeader("content-type", "application/json");
             request.setEntity(params);
             HttpResponse response = httpClient.execute(request);
 
-            System.out.println(response);
+            System.out.println(response.getStatusLine());
         } catch (Exception ex) {
             System.err.println(ex.getMessage());
         }
--- src/main/java/hints/main.java
+++ src/main/java/hints/main.java
@@ -1,4 +1,11 @@
 package hints;
 
+import score.Score;
+
 public class main {
+    public static void main(String[] args) {
+        Highscores highscores = new HighscoresHTTP();
+
+        highscores.addScore(new Score("aaa", 111111111));
+    }
 }
