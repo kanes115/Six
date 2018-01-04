f25d3d8ded2aee87c03f4187e897d235a34d63d1
kanes115 <dominikstanaszek@icloud.com>
2018-01-04 18:39:00 +0100

    Remove unnecessary class

--- src/main/java/game/GameController.java
+++ src/main/java/game/GameController.java
@@ -4,14 +4,12 @@ package game;
 public class GameController {
 
     private  MoveChecker checker;
-    private ExecutedMovesStack movesStack;
     private Board board;
     private State state;
 
 
-    public GameController(ExecutedMovesStack moveStack, MoveChecker checker, Board board, State state){
+    public GameController(MoveChecker checker, Board board, State state){
         this.checker = checker;
-        this.movesStack = moveStack;
         this.board = board;
         this.state = state;
     }
--- src/test/java/game/GameCheckerTests.groovy
+++ src/test/java/game/GameCheckerTests.groovy
@@ -12,10 +12,9 @@ class GameCheckerTests extends Specification  {
 
     def "should return true if move is  correct"() {
         given:
-        ExecutedMovesStack movesStack = Stub()
         Board board = Stub()
         MoveChecker checker = Stub()
-        GameController gameController = new GameController(movesStack,checker,board,State.INPROGRESS)
+        GameController gameController = new GameController(checker,board,State.INPROGRESS)
         Move move = Stub()
         checker.isCorrect(move) >> true
         expect:
