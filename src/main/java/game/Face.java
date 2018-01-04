87141a6d2b1116210bab9526098ba07b6949de0f
kanes115 <dominikstanaszek@icloud.com>
2018-01-04 20:13:19 +0100

    Add isRemovable method

--- src/main/java/game/Face.java
+++ src/main/java/game/Face.java
@@ -4,5 +4,9 @@ package game;
  * Created by Kanes on 05.12.2017.
  */
 public enum Face {
-    TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, JACK, QUEEN, KING, ACE;   //TODO: fill up
+    TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, JACK, QUEEN, KING, ACE;
+
+    public boolean isRemovable(){
+        return ordinal() < 4 || ordinal() == 11;
+    }
 }
