f1edcda9702f19346cb308cf1908f8876c37ec0b
kanes115 <dominikstanaszek@icloud.com>
2018-01-04 20:59:19 +0100

    Add helping methods to Face enum

--- src/main/java/game/Face.java
+++ src/main/java/game/Face.java
@@ -1,8 +1,18 @@
 package game;
 
+import com.sun.istack.internal.NotNull;
+
 /**
  * Created by Kanes on 05.12.2017.
  */
 public enum Face {
-    TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, JACK, QUEEN, KING, ACE;   //TODO: fill up
+    TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, JACK, QUEEN, KING, ACE;
+
+    public boolean isRemovable(){
+        return ordinal() < 4 || ordinal() == 11;
+    }
+
+    public Face next(@NotNull Face f){
+        return values()[(f.ordinal() + 1) % 12];
+    }
 }
