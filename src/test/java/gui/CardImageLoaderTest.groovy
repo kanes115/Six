83adccce0cbfa6b1eaa9dcff6fb276489a88b115
Michał Grzesik <mgrzesik7@gmail.com>
2018-01-05 14:59:02 +0100

    Added test to verify whether card images are loaded correctly

--- src/test/java/gui/CardImageLoaderTest.groovy
+++ src/test/java/gui/CardImageLoaderTest.groovy
@@ -5,15 +5,14 @@ import spock.lang.Specification
 
 class CardImageLoaderTest extends Specification {
 
-    def "Check whether all images path are correctly and allow to load appropriate image"(){
+    def "Check whether all images path are correctly and allow to load appropriate image"() {
         given:
         List<Card> cards = Card.values()
         when:
-        for (Card card :cards){
+        for (Card card : cards) {
             new Image(getClass().getResourceAsStream(card.getPathToFilename()))
         }
         then:
         notThrown(Exception)
-
     }
 }
