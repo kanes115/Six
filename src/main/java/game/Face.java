d4a00697265e138611f5e89b590bd59073f2132c
kanes115 <dominikstanaszek@icloud.com>
2018-01-10 20:48:59 +0100

    Add missing face

--- src/main/java/game/Face.java
+++ src/main/java/game/Face.java
@@ -4,7 +4,7 @@ package game;
  * Created by Kanes on 05.12.2017.
  */
 public enum Face {
-    TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, JACK, QUEEN, KING, ACE;
+    TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE;
 
     public boolean isUnnecessary(){
         return ordinal() < 4 || ordinal() == 11;
