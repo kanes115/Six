d50c43d6eae0813b09fb0b2b8cd9e44412c5c3e6
kanes115 <dominikstanaszek@icloud.com>
2018-01-12 13:14:56 +0100

    Fix Board tests and add some

--- src/test/java/game/BoardTest.groovy
+++ src/test/java/game/BoardTest.groovy
@@ -6,33 +6,114 @@ class BoardTest extends Specification {
 
     def "After removing cards, positions are empty"(){
         given:
-        Board b = new Board()
+        CardShuffler shuffler = new RandomShuffler()
+        Board board = new Board(shuffler)
         when:
-        def Pos1 = b.getPositionAt(0,0)
-        def Pos2 = b.getPositionAt(1,1)
-        b.removeCards(Pos1, Pos2)
+        def Pos1 = board.getPositionAt(0,0)
+        def Pos2 = board.getPositionAt(1,1)
+        board.removeCards(Pos1, Pos2)
         then:
-        b.getPositionAt(0, 0).isEmpty()
-        b.getPositionAt(1, 1).isEmpty()
+        board.getPositionAt(0, 0).isEmpty()
+        board.getPositionAt(1, 1).isEmpty()
     }
 
     def "Shuffler decides about what cards are put where on the table"(){
         given:
-        CardShuffler s = Stub()
         def rest = new LinkedList<Card>()
-        rest.add(new Card(Color.DIAMONDS, Face.ACE))
-        rest.add(new Card(Color.DIAMONDS, Face.ACE))
+        rest << new Card(Color.DIAMONDS, Face.ACE)
+        rest << new Card(Color.DIAMONDS, Face.ACE)
 
-        s.getNextCard() >>> new Card(Color.CLUBS, Face.FOUR)
-        s.getRestCards() >>> rest
+        CardShuffler shuffler = getOneCardShuffler(new Card(Color.DIAMONDS, Face.ACE), rest)
 
         when:
-        def b = new Board(s)
+        def board = new Board(shuffler)
         then:
-        b.getPositionAt(0, 0).getCard().getColor() == Color.CLUBS
-        b.getPositionAt(0, 0).getCard().getFace() == Face.FOUR
-        b.getDeckPosition().getCard().getColor() == Color.DIAMONDS
-        b.getDeckPosition().getCard().getFace() == Face.ACE
+        board.getPositionAt(0, 0).getCard().getColor() == Color.CLUBS
+        board.getPositionAt(0, 0).getCard().getFace() == Face.FOUR
+        board.getDeckPosition().getCard().getColor() == Color.DIAMONDS
+        board.getDeckPosition().getCard().getFace() == Face.ACE
+    }
+
+    def "areAllCardsInPlace methods return false if they are not 6 - King and not on color-per-row basis"(){
+        given:
+        CardShuffler shuffler = getOneCardShuffler(new Card(Color.DIAMONDS, Face.ACE))
+
+        when:
+        def board = new Board(shuffler)
+
+        then:
+        !board.areAllCardsInPlace()
+        !board.areAllRowsAssigned() //this also can't be fulfilled here
+    }
+
+    def "Board with randomShuffler (that gives a proper amount of cards never has free positions at the beginning"(){
+        given:
+        CardShuffler shuffler = getOneCardShuffler(new Card(Color.DIAMONDS, Face.ACE))
+
+        when:
+        def board = new Board(shuffler)
+
+        then:
+        !board.hasFreePositions()
+    }
+
+    def "getPositionAt(row, col) return IllegalArgumentException when indexes out of bounds (row: 0-3, col: 0-5)"(){
+        given:
+        CardShuffler shuffler = getOneCardShuffler(new Card(Color.DIAMONDS, Face.ACE))
+        def board = new Board(shuffler)
+
+        when:
+        board.getPositionAt(100, 100)
+        then:
+        thrown IllegalArgumentException
+
+        when:
+        board.getPositionAt(4, 6)
+        then:
+        thrown IllegalArgumentException
+
+        when:
+        board.getPositionAt(2, 6)
+        then:
+        thrown IllegalArgumentException
+
+        when:
+        board.getPositionAt(4, 2)
+        then:
+        thrown IllegalArgumentException
+
+        when:
+        board.getPositionAt(-1, -1)
+        then:
+        thrown IllegalArgumentException
+
+        when:
+        board.getPositionAt(2, 2)
+        then:
+        notThrown IllegalArgumentException
+
+    }
+
+
+    // Gives a shuffler that will make board have:
+    // on matrix only `card`, on deck `rest`
+    def getOneCardShuffler(Card card, List<Card> rest){
+        CardShuffler shuffler = Stub(CardShuffler.class)
+
+        shuffler.getRestCards() >> rest
+
+        List<Card> row = new LinkedList<>()
+        for(int i = 0; i < 8; i ++)
+            row << new Card(Color.CLUBS, Face.FOUR)
+
+        shuffler.getNextCards(8) >> row
+        return shuffler
+    }
+    def getOneCardShuffler(Card card){
+        def rest = new LinkedList<Card>()
+        rest << new Card(Color.DIAMONDS, Face.ACE)
+        rest << new Card(Color.DIAMONDS, Face.ACE)
+        return getOneCardShuffler(card, rest)
     }
 
 
