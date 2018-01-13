fe21992e82bc5eeb518f581aacf33431c0999f24
kanes115 <dominikstanaszek@icloud.com>
2018-01-13 14:17:17 +0100

    Add a move that assigns row a color
    This move should be used to assign a color to row when a card
    is already in a proper place.

--- src/main/java/game/Moves/AssignColorOnCard.java
+++ src/main/java/game/Moves/AssignColorOnCard.java
@@ -1,4 +1,51 @@
 package game.Moves;
 
-public class AssignColorOnCard {
+import game.Board;
+import game.Positions.CasualPosition;
+import game.Row;
+
+public class AssignColorOnCard implements Move {
+
+
+    private final CasualPosition pos;
+    private final Board board;
+    private String errorMsg;
+    private boolean isMade = false;
+
+    public AssignColorOnCard(CasualPosition pos){
+        this.pos = pos;
+        this.board = pos.getBoard();
+    }
+
+    @Override
+    public boolean execute() {
+        Row targetRow = pos.getRow();
+        if(targetRow.isColorAssigned())
+            return error("This card is already in assigned row");
+        if(board.getAssignedColors().contains(pos.getCard().getColor()))
+            return error("This card has a row specified already");
+        pos.getRow().assignColor(pos.getCard().getColor());
+        return true;
+    }
+
+    @Override
+    public void revert() {
+
+    }
+
+    @Override
+    public boolean isMade() {
+        return this.isMade;
+    }
+
+    @Override
+    public String getErrorMessage() {
+        return this.errorMsg;
+    }
+
+    private boolean error(String msg){
+        this.errorMsg = msg;
+        this.isMade = false;
+        return false;
+    }
 }
