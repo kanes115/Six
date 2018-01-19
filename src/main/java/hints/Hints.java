a8e377bcf86f716bc01524fae366f84e9fda84ba
Jakub Kret <jakub.a.kret@gmail.com>
2018-01-19 14:52:37 +0100

    Add separate methods to return Moves and Positions

--- src/main/java/hints/Hints.java
+++ src/main/java/hints/Hints.java
@@ -1,8 +1,6 @@
 package hints;
 
 import game.Board;
-import game.Card;
-import game.GameController;
 import game.Moves.Move;
 import game.Moves.MoveFactory;
 import game.Positions.CasualPosition;
@@ -11,6 +9,7 @@ import game.Row;
 
 import java.util.LinkedList;
 import java.util.List;
+
 //TODO compress duplicated logic checks in all the ifs
 public class Hints {
 
@@ -25,26 +24,66 @@ public class Hints {
         return position.getCard().equals(position1.getCard()) && position.getCard().getFace().isUnnecessary();
     }
 
-    public List<Move> getUnnecessaryDups() {
+    public List<Move> getUnnecessaryDupsMoves() {
         List<Row> rows = board.getRows();
         List<Move> dups = new LinkedList<>();
-        Row row = rows.get(0);
-        for (CasualPosition position : row.getPositions()) {
-            for (int i = 1; i < 4; i++) {
-                Row checkRow = rows.get(i);
-                for(CasualPosition position1 : checkRow.getPositions()) {
-                    if (checkIfDups(position, position1)) {
-                        dups.add(MoveFactory.getMove(position, position1, true));
+        for (int i = 0; i < 4; i++) {
+            Row baseRow = rows.get(i);
+            for (CasualPosition position : baseRow.getPositions()) {
+                if (!position.isEmpty()) {
+                    for (int j = i + 1; j < 4; j++) {
+                        Row checkRow = rows.get(j);
+                        for (CasualPosition position1 : checkRow.getPositions()) {
+                            if (!position1.isEmpty()) {
+                                if (checkIfDups(position, position1)) {
+                                    dups.add(MoveFactory.getMove(position, position1, true));
+                                }
+                            }
+                        }
+                    }
+                    if (!board.getRejectedPosition().isEmpty()) {
+                        if (checkIfDups(position, board.getRejectedPosition())) {
+                            dups.add(MoveFactory.getMove(position, board.getRejectedPosition(), true));
+                        }
+                    }
+                    if (!board.getDeckPosition().isEmpty()) {
+                        if (checkIfDups(position, board.getDeckPosition())) {
+                            dups.add(MoveFactory.getMove(position, board.getDeckPosition(), true));
+                        }
                     }
                 }
             }
-            if(!board.getRejectedPosition().isEmpty()) {
-                if (checkIfDups(position, board.getRejectedPosition())){
-                    dups.add(MoveFactory.getMove(position, board.getRejectedPosition(), true));
-                }
-            } else {
-                if (checkIfDups(position, board.getDeckPosition())) {
-                    dups.add(MoveFactory.getMove(position, board.getDeckPosition(), true));
+        }
+        return dups;
+    }
+
+    public List<Position[]> getUnnecessaryDupsPositions() {
+        List<Row> rows = board.getRows();
+        List<Position[]> dups = new LinkedList<>();
+        for (int i = 0; i < 4; i++) {
+            Row baseRow = rows.get(i);
+            for (CasualPosition position : baseRow.getPositions()) {
+                if (!position.isEmpty()) {
+                    for (int j = i + 1; j < 4; j++) {
+                        Row compareRow = rows.get(j);
+                        for (CasualPosition position1 : compareRow.getPositions()) {
+                            if (!position1.isEmpty()) {
+                                if (position.getCard().equals(position1.getCard()) &&
+                                        position.getCard().getFace().isUnnecessary()) {
+                                    dups.add(new Position[]{position, position1});
+                                }
+                            }
+                        }
+                    }
+                    if (!board.getRejectedPosition().isEmpty() &&
+                            position.getCard().getFace().isUnnecessary() &&
+                            board.getRejectedPosition().getCard().equals(position.getCard())) {
+                        dups.add(new Position[]{position, board.getRejectedPosition()});
+                    } else if (!board.getDeckPosition().isEmpty() &&
+                            position.getCard().getFace().isUnnecessary() &&
+                            board.getDeckPosition().getCard().equals(position.getCard())) {
+                        dups.add(new Position[]{position, board.getDeckPosition()});
+                    }
                 }
             }
         }
@@ -56,7 +95,7 @@ public class Hints {
         return (!position1.isEmpty() && position.getTargetFace() == position1.getCard().getFace() && (!row.isColorAssigned() || row.getColor() == position.getCard().getColor()));
     }
 
-    public List<Move> showActionable() {
+    public List<Move> showActionableMoves() {
         MoveFactory moveFactory = new MoveFactory();
         List<Move> moves = new LinkedList<>();
         board.getRows().forEach(row -> {
@@ -64,17 +103,17 @@ public class Hints {
                 if (casualPosition.isEmpty()) {
                     board.getRows().forEach(row1 -> {
                         row1.getPositions().forEach(casualPosition1 -> {
-                            if(cardCanGo(row, casualPosition, casualPosition1)) {
-                                moves.add(MoveFactory.getMove(casualPosition1 ,casualPosition, false));
+                            if (cardCanGo(row, casualPosition, casualPosition1)) {
+                                moves.add(MoveFactory.getMove(casualPosition1, casualPosition, false));
                             }
                         });
                     });
-                    if(!board.getRejectedPosition().isEmpty()) {
-                        if(cardCanGo(row, casualPosition, board.getRejectedPosition())) {
-                            moves.add(MoveFactory.getMove(board.getRejectedPosition() ,casualPosition, false));
+                    if (!board.getRejectedPosition().isEmpty()) {
+                        if (cardCanGo(row, casualPosition, board.getRejectedPosition())) {
+                            moves.add(MoveFactory.getMove(board.getRejectedPosition(), casualPosition, false));
                         }
                     } else {
-                        if(cardCanGo(row, casualPosition, board.getDeckPosition())) {
+                        if (cardCanGo(row, casualPosition, board.getDeckPosition())) {
                             moves.add(MoveFactory.getMove(board.getDeckPosition(), casualPosition, false));
                         }
                     }
@@ -82,6 +121,29 @@ public class Hints {
             });
         });
         return moves;
+    }
+
+    public List<Position> showActionablePositions() {
+        List<Position> actionable = new LinkedList<>();
+        board.getRows().forEach(row -> {
+            row.getPositions().forEach(casualPosition -> {
+                if (casualPosition.isEmpty()) {
+                    board.getRows().forEach(row1 -> {
+                        row1.getPositions().forEach(casualPosition1 -> {
+                            if (!casualPosition1.isEmpty() && casualPosition.getTargetFace() == casualPosition1.getCard().getFace() && (!row.isColorAssigned() || row.getColor() == casualPosition1.getCard().getColor())) {
+                                actionable.add(casualPosition1);
+                            }
+                        });
+                    });
+                    if (!board.getRejectedPosition().isEmpty() && casualPosition.getTargetFace() == board.getRejectedPosition().getCard().getFace() && (!row.isColorAssigned() || row.getColor() == board.getRejectedPosition().getCard().getColor())) {
+                        actionable.add(board.getRejectedPosition());
+                    } else if (casualPosition.getTargetFace() == board.getDeckPosition().getCard().getFace() && (!row.isColorAssigned() || row.getColor() == board.getDeckPosition().getCard().getColor())) {
+                        actionable.add(board.getDeckPosition());
+                    }
+                }
+            });
+        });
+        return actionable;
     }
 
     private boolean checkIfCardInPlace(CasualPosition position, Row row) {
@@ -92,15 +154,15 @@ public class Hints {
         return !position.equals(position1) && !position1.isEmpty() && position.getCard().equals(position1.getCard());
     }
 
-    public List<Move> deletableDuplicates() {
+    public List<Move> deletableDuplicatesMoves() {
         List<Row> rows = board.getRows();
         List<Move> actionable = new LinkedList<>();
         rows.forEach(row -> {
             row.getPositions().forEach(casualPosition -> {
-                if(checkIfCardInPlace(casualPosition, row)) {
+                if (checkIfCardInPlace(casualPosition, row)) {
                     rows.forEach(row1 -> {
                         row1.getPositions().forEach(casualPosition1 -> {
-                            if(checkIfDuplicate(casualPosition, casualPosition1)) {
+                            if (checkIfDuplicate(casualPosition, casualPosition1)) {
                                 actionable.add(MoveFactory.getMove(casualPosition, true));
                             }
                         });
@@ -111,10 +173,29 @@ public class Hints {
         return actionable;
     }
 
+    public List<CasualPosition> deletableDuplicatesPositions() {
+        List<Row> rows = board.getRows();
+        List<CasualPosition> actionable = new LinkedList<>();
+        rows.forEach(row -> {
+            row.getPositions().forEach(casualPosition -> {
+                if (row.isColorAssigned() && !casualPosition.isEmpty() && row.getColor() == casualPosition.getCard().getColor() && casualPosition.cardFaceMatchPosition()) {
+                    rows.forEach(row1 -> {
+                        row1.getPositions().forEach(casualPosition1 -> {
+                            if (!casualPosition.equals(casualPosition1) && !casualPosition1.isEmpty() && casualPosition.getCard().equals(casualPosition1.getCard())) {
+                                actionable.add(casualPosition1);
+                            }
+                        });
+                    });
+                }
+            });
+        });
+        return actionable;
+    }
+
     public boolean movesLeft() {
-        List<Move> unnecessary = getUnnecessaryDups();
-        List<Move> actionable = showActionable();
-        List<Move> dups = deletableDuplicates();
+        List<Move> unnecessary = getUnnecessaryDupsMoves();
+        List<Move> actionable = showActionableMoves();
+        List<Move> dups = deletableDuplicatesMoves();
 
         return unnecessary.size() + actionable.size() + dups.size() > 0;
     }
