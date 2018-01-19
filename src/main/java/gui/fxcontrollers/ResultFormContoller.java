9835a32a819c9f7db8264e7e46f4e927ce58c484
Michał Grzesik <mgrzesik7@gmail.com>
2018-01-19 05:27:31 +0100

    Attempt to integration with score

---
+++ src/main/java/gui/fxcontrollers/ResultFormContoller.java
@@ -0,0 +1,61 @@
+package gui.fxcontrollers;
+
+import game.GameController;
+
+import gui.GuiTools;
+import gui.Main;
+import gui.dictionary.AppConstants;
+import hints.Highscores;
+import hints.HighscoresHTTP;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import score.Score;
+
+/**
+ * Created by michaello on 19.01.18.
+ */
+public class ResultFormContoller {
+
+    public TextField txtUsername;
+    public Label resultContent;
+    public Label resultLabel;
+    public Label timeLabel;
+    public Label timeContent;
+    private final GameController gameController = GamePaneController.getGameController();
+    private final HighscoresHTTP highscoresHTTP = new HighscoresHTTP();
+
+    @FXML
+    public void handleSendButtonAction(ActionEvent actionEvent) {
+        if(txtUsername.getText().isEmpty()){
+            GuiTools.showAlertDialog("Brak nazwy uzytkownika", "Podaj nazwe uzytkownika", null);
+            return;
+        }
+        highscoresHTTP.addScore(new Score(txtUsername.getText(), 0 ));
+        replaceStage();
+    }
+
+    @FXML
+    public void handleAbortButtonAction(ActionEvent actionEvent) {
+
+        replaceStage();
+    }
+
+    @FXML
+    public void initialize(){
+
+        //  resultContent.setText();
+        timeContent.setText(String.valueOf(gameController.getTime()));
+    }
+
+    private void replaceStage(){
+        try {
+            Main.replaceStage(AppConstants.INTRO_STAGE_URL);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
